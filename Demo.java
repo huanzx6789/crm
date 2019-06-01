f77af699c45134cf44687f14605f52fc42f62a5b
mldn <mldnqa@163.com>
2019-06-01 11:24:51 +0800

    Modify Demo.java

--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@
 public class Demo {
 	private String str = "hello" ;
 	private String orstr = new String("hello") ;
+	//--------------test----
 	public Demo() {
 		System.out.println(str.equals(orstr)) ;
 	}
