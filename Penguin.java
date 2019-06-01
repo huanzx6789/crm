d88b299c41113c461bf47f834a1370e64dc6b83c
mldn <mldnqa@163.com>
2019-06-01 15:50:28 +0800

    Add Penguin.java

---
+++ Penguin.java
@@ -0,0 +1,19 @@
+
+
+public class Penguin {
+	private String name ;
+	private int id ;
+	public Penguin(String myName, int id) {
+		name = myName ;
+		id = myid ;
+	}
+	public void eat() {
+		System.out.println(name + "eatind") ;
+	}
+	public void sleep() {
+		System.out.println(name + "sleeping")
+	}
+	public void introduction() {
+		System.out.println("good night!" + id + ":" + name) ;
+	}
+}
