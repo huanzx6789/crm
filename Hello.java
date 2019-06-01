a9866a2c297538b564022a8e1356f1e27f5d2421
mldn <mldnqa@163.com>
2019-06-01 10:08:23 +0800

    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+
+
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("分支管理") ;
+		System.out.println("branch") ;
+		System.out.println("checkout branchname") ;
+	}
+}
